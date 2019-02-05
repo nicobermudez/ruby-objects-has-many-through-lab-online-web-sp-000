5efd839d816ac59239a16f1e32c807d92ed488b3
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-05 18:04:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,19 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :name, :genre
 
 @@all = []
 
 def initialize(name)
   @name=name
   @@all << self
+end
+
+def self.all
+  @@all
+end
+
+def new_song(name, genre)
+  Song.new(name, self, genre)
 end
 
 end
