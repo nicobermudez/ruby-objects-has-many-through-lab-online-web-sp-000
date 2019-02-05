52a792bd9fce6d9ef6dc7812fd9dabef50158d7c
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-05 18:21:23 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,26 +1,23 @@
 class Artist
   attr_accessor :name
 
-@@all = []
+  @@all = []
 
-def initialize(name)
-  @name=name
-  @@all << self
-end
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
 
-def self.all
-  @@all
-end
+  def self.all
+    @@all
+  end
 
-def new_song(name, genre)
-  Song.new(name, self, genre)
-end
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+  end
 
-def songs
-  Song.all.select {|song| song.artist == artist}
-end
-
-def genres
-end
+  def songs
+    Song.all.select { |song| song.artist == self }
+  end
 
 end
