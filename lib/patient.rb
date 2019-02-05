ae52250fb52f07be3d046848d67a7a08e3c16876
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-05 18:41:29 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -16,11 +16,11 @@ class Patient
     Appointment.new(self, doctor, date)
   end
 
-  def appointments 
+  def appointments
     Appointment.all.select {|appointment| appointment.patient == self}
   end
 
-  def doctors 
+  def doctors
     appointments.collect {|appointment| appointment.doctor}
   end
 
