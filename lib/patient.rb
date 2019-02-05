163bf5c08e98d14747ae0789f7e77bbf755fa214
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-05 18:33:17 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,16 @@
+class Patient 
+  attr_accessor :name, :doctor, :date 
+
+  @@all = []
+
+  def self.all 
+    @@all 
+  end 
+
+  def initialize(name)
+    @name=name 
+    @@all << self 
+  end
+
+
+end
